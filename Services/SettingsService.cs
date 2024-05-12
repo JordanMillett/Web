a3c01ef80a75fb74743acb4c28806b320e3754ce
Jordan Millett <JelloJordan3@gmail.com>
2024-05-12 15:48:40 -0700

    Update

---
+++ Services/SettingsService.cs
@@ -0,0 +1,13 @@
+namespace Web
+{
+    public partial class SettingService
+    {
+        public string Sentences = "easy";
+        public string Vocab = "easy";
+        
+        public SettingService()
+        {
+            Console.WriteLine("Settings Initialized");
+        }
+    }
+}
