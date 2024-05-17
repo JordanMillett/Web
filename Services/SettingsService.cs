bec254e251b0f7ab105f8ae0b5dfbfb9616c5c51
Jordan Millett <JelloJordan3@gmail.com>
2024-05-16 22:41:46 -0700

    Update

--- Services/SettingsService.cs
+++ Services/SettingsService.cs
@@ -3,7 +3,7 @@ namespace Web
     public partial class SettingService
     {
         public string Sentences = "easy";
-        public string Vocab = "hard";
+        public string Vocab = "large";
         public bool SpokenEnglish = false;
 
         public List<string> Sentence_Options = new List<string>()
